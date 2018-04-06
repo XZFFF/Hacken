c538785df95e17c94678a6e117345daeee79282f
wang <108474309@qq.com>
2018-04-07 07:57:42 +0800

    底栏样式修改

---
+++ Hacken/hackenpages/hacken/js/common.js
@@ -0,0 +1,12 @@
+var part1=document.getElementById("part1");
+var part2=document.getElementById("part2");
+var part3=document.getElementById("part3");
+part1.onclick=function  () {
+	mui.openWindow("../model1_hacker/hacker_list.html")
+}
+part2.onclick=function  () {
+	mui.openWindow("../model2_idea/idea_list.html")
+}
+part3.onclick=function  () {
+	mui.openWindow("../model3_mine/mine_info.html")
+}
